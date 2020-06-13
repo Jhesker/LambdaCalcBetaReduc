3f22d5f4fef13242f35751808c41b7765ef632cf
jhesker <jhesker@regis.edu>
2020-06-13 15:57:20 -0500

    work done on parseAbstraction

--- src/exceptions/ParseException.java
+++ src/exceptions/ParseException.java
@@ -6,8 +6,8 @@ import lambdaexpr.ExprKind;
  * @author jhesker
  */
 public class ParseException extends Exception{
-     public ParseException(ExprKind type, int lineNo) {
-        this("Expecting " + type + " at " + lineNo);
+     public ParseException(ExprKind type, char found, char charExp ) {
+        this(type + "Expects a "  + charExp + " at " + found);
     }
     public ParseException(String msg){
         super(msg);
--- src/lambdaexpr/Abstraction.java
+++ src/lambdaexpr/Abstraction.java
@@ -11,6 +11,7 @@ public class Abstraction implements LambdaExpr{
     
     //contructor
     public Abstraction() {
+        
     }
 
     
